ac40ec45ad35bc0ae2aafcea1f2c64c3f4c34584
Tiwari Ajay Virendra <ajaytripathi821@gmail.com>
2025-08-03 11:03:39 +0530

    Practice of some question on what i learned in these 3 classes

---
+++ PracticeOneTwoThreeClass.py
@@ -0,0 +1,75 @@
+# Statement: 1
+# Write a program that takes an integer input and prints whether the number is positive, negative, or zero.
+
+# num= int(input("Enter number 1"))
+
+# if(num  > 0):
+#     print("Positive")
+# elif(num < 0 ):
+#     print("Negative")
+# else:
+#     print("zero")
+
+
+# Statement: 2
+# Given two lists, check if they are equal (same elements in the same order). If not, print which one is greater based on length.
+
+# list1 =[1,4,8,9,7]
+# list2 =[2,4,8,1,4,5]
+
+# if(list1 == list2):
+#     print("This is Same Elements in the same order")
+# elif(len(list1) > len(list2 )):
+#     print("list 1 is Greater ")
+# else:
+#     print("list 2 is greater")
+
+
+# Statement:3 
+# Ask the user for their age and check if they are eligible to vote (age ≥ 18). If not, print how many years are left.
+
+# age = int(input("What is your age"))
+
+# if(age >= 18):
+#     print("You are Eligible")
+# else:
+#     print("You are not Eligible", 18 - age, " year left to vote")
+
+
+#  Problem 4: Find the Largest of Three Numbers
+# Statement:
+# Take three numbers as input and print the largest one.
+
+
+# a= int(input("Enter First number"))
+# b= int(input("enter 2nd Number"))
+# c= int(input("enter 3rd number"))
+
+# if(a >= b and a >= c):
+#     print("A is Greatest number")
+# elif(b >= c):
+#     print("B is the Greatest number")
+# else:
+#     print("C is greaest number")
+
+# Problem 5: Check if a List is Empty or Not
+# Statement:
+# Write a program that checks if a list is empty or not empty.
+
+# list = []
+
+# if(list != []):
+#     print("list is not empty")
+
+# else:
+#     print("list is empty")
+
+
+# sol 2  
+
+# my_list = []
+
+# if not my_list:
+#     print("The list is empty.")
+# else:
+#     print("The list is not empty.")
---
+++ ThirdProgrmLact3.py
@@ -0,0 +1,86 @@
+# List and Tuple
+
+# marks=[45.5, 54.5, 65.3, 74.3]
+# print(marks)
+# print(type(marks))
+# print(marks[3])
+
+
+# list Slicing 
+
+
+# sublist
+
+# marks= [23,54,24,53,64]
+# print(marks[1:4])
+
+# negatinve indexing
+
+# marks=[23,52,51,63,65]
+# print(marks[-4:-1])
+
+# List Methods in python
+
+# Append // means  to add something on last
+
+# list =[2, 52,5,]
+# list.append(4)
+# print(list)
+
+
+# # List sort 
+
+# assending Order .
+
+
+# list= [45,34,46,23]
+# print(list.append(41))
+# print(list.sort())
+# print(list)
+
+
+# decending order 
+
+# list= [45,34,46,23]
+# print(list.append(41))
+# print(list.sort(reverse=True))
+# print(list)
+
+# string Shorting
+
+# assending order
+
+# list =['e','a','d','c','b']
+# print(list.sort(reverse=True))
+# print(list)
+
+# # decending order
+
+# List=['e','a','d','c','b']
+# print(list.sort())
+# print(list)
+
+# list=['e','a','d','c','b']
+# list.reverse()
+# print(list)
+
+# # Insert
+
+# list =[2, 3,1]
+# list.insert(2,5)
+# print(list)
+
+
+# remove
+# list=[2,1,3,1]
+# list.remove(3)
+# print(list)
+# # Pop
+# list=[4,7,6,7]
+# list.pop(1)
+# print(list)
+
+
+
+# Tuple in this we use Parenthises () = Tuple 
+
