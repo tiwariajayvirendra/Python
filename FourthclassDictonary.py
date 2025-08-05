54fc4f64bb9c34122fd15cee8697d833a06d0ac2
Tiwari Ajay Virendra <ajaytripathi821@gmail.com>
2025-08-05 12:33:42 +0530

    Today i learned Sets, and its some useful methods , like dictonay, methods , update, add, etc

---
+++ FourthclassDictonary.py
@@ -0,0 +1,127 @@
+# dect = {
+#     "name":"Ajay Tiwari",
+#     "sub": "Computer Science ",
+#     "age":"25",
+#     "married":"Not Married"
+# }
+
+# # updating new value on dictonary
+
+# dect["surname"] = "tripathi"
+# print(dect)
+
+
+# Null dict
+
+# null_dict={}
+
+# null_dict["name"]="Ajay"
+# print(null_dict)
+
+# Nested Dictonary in python
+
+# info = {
+#     "name":"Ajay ",
+#     "age":"25",
+#     "sub":{
+#         "math":"36",
+#         "hindi":"45",
+#         "computer":"48"
+#     }
+     
+# }
+
+# print(info["sub"]["hindi"])
+
+
+# typecast in list 
+# print(list(info.keys()))
+
+
+# # length of the dict
+
+# print(len(info))
+
+# .values Method
+
+# print(info.items())
+
+ 
+# # print(info.get("name"))   it is very important use this for print next code to like this
+
+# print("Befor")
+# print(info("name2"))
+# print("after")
+
+
+# UPDATE method 
+
+# new_dict={
+# "work":"Coding"
+# }
+
+# info.update(new_dict)
+# print(info)
+
+
+# Set topic\
+
+# collection = {1,3,2,"ajay","vijay"}
+
+# print(collection)
+# print(type(collection))
+
+# # it will ignor dublicate values like this
+
+# collection = {1,3,2,1,"ajay","vijay",2}
+
+# print(len(collection))
+# print(type(collection))
+
+# How to create empty set 
+
+# collection = set()
+
+# print(collection)
+# print(type(collection))
+
+# collection.add(1)
+ 
+# collection.add(2)
+# collection.add("akau")
+# collection.add((1,3,4))
+
+# # print(collection)
+
+# # collection.remove(1)
+
+# # clear method
+# collection.clear()
+# print(len(collection))
+
+# Pop method
+
+# collection={"ajau","alau", "akahand","arau","dkdie"}
+
+# print(collection.pop())
+# print(collection.pop())
+
+# we will pass diffrent types of values like Tuple , string, 
+
+# Union Method :it combaines other values
+ 
+# set1= {1,2,3}
+# set2= {3,4,5}
+
+# print(set1.union(set2))
+
+# # output(1,2,3,4,5)
+
+# print(set1.intersection(set2))
+
+
+
+# Problem Statements
+
+# SOlutions
+
